3e829a173ee3ae7766bc775e1fde1570ebeadcdf
Ethan Tucker <ectucker1@gmail.com>
2019-11-09 12:13:16 -0600

    Particle effects for fireball and trail

--- Core/Spells/Fireball.gd
+++ Core/Spells/Fireball.gd
@@ -3,3 +3,5 @@ extends Spatial
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	translate(transform.basis.z.normalized() * delta * 5.0)
+	if transform.origin.length() > 10:
+		queue_free()
