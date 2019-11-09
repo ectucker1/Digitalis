f2c777a173124ce090ecc4d0a59fc5ef227e2611
Matthew Tucker <mct4800@gmail.com>
2019-11-09 10:04:52 -0600

    Added basic GUI elements (broken slider, working console window)

---
+++ Label.gd
@@ -0,0 +1,7 @@
+extends Label
+#printline prints a line of your choosing.
+func printline(x):
+	set_text(get_text() + "\n" + x)
+#cleartext clears the text from the previous frame.
+func cleartext():
+	set_text("")
